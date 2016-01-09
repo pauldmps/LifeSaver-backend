8117bc2663e5fbb86fe2f20aa87013e59c1bc31b
Shantanu Paul <shantanupaul@live.com>
2016-01-10 00:33:16 +0530

    Deploy

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ var tokenController = require('./Controllers/validate');
     + process.env.OPENSHIFT_MONGODB_DB_HOST + ':'
     + process.env.OPENSHIFT_MONGODB_DB_PORT + '/lifesaver'); */
 
-mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + 'lifesaver'|| 'mongodb://127.0.0.1:27017/test');
+mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + 'lifesaver');
+//mongoose.connect('mongodb://127.0.0.1:27017/test');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
