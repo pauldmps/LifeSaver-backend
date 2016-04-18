5771de82f092ce47d66d49f910e25dd8f8ce4e51
Shantanu Paul <shantanupaul@live.com>
2016-04-19 00:48:03 +0530

    Deploy

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.get('/auth/nearbyUsers',userController.getNearbyUsers);
 app.post('/auth/profilePic',upload.single('profilepic'),function(req,res){
         console.log(req.file);
         userController.setProfilePic(req,res);
+        console.log('setprofilepic');
 });
 app.get('/auth/profilePic',userController.getProfilePic);
 
